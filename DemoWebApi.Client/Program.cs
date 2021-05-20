cad660f185dd67cb2d55ffb10c61c87fd9c2a35d
Ali MAKRI <microsoft@makri.fr>
2021-05-20 09:38:26 +0200

    09:38

--- DemoWebApi.Client/Program.cs
+++ DemoWebApi.Client/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,30 @@ namespace DemoWebApi.Client
     {
         static void Main(string[] args)
         {
-            // 
+            // https://restcountries.eu/rest/v2/all
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://restcountries.eu/rest/v2/");
+            var reponse = client.GetAsync("all");
+            reponse.Wait();
+            var result = reponse.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var reader = result.Content.ReadAsStringAsync();
+                reader.Wait();
+
+                var data = JsonConvert.DeserializeObject<Pays[]>(reader.Result);
+
+                foreach (var pays in data.Where(p => p.Region == "Africa"))
+                {
+                    Console.WriteLine(pays.Name);
+                }
+                Console.ReadLine();
+            }
         }
+    }
+    public class Pays
+    {
+        public string Name;
+        public string Region;
     }
 }
